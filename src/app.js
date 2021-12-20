677e0e16b3f07c72603ea14afaa21807fc2749e8
henrique2019 <henriqueronald2016@gmail.com>
2021-12-20 02:56:59 +0000

    origin

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 
 
 app.use(function(req, res, next) {
-        res.header({ "Access-Control-Allow-Origin": "*"}); // permite que pessoas acessem a api
+        res.header({ "Access-Control-Allow-Origin": "https://producaomilgrau.vercel.app/"}); // permite que pessoas acessem a api
         res.header({"Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token, Accept"});
         res.header({"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",});
         next();
